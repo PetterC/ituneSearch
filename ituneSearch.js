71336c016a93288e48657478d5d56fc4dbd7b014
PetterC <petterchan89@gmail.com>
2016-12-15 08:53:09 -0600

    Add files via upload

---
+++ ituneSearch.js
@@ -0,0 +1,22 @@
+var iTunesController = function($scope, $http){
+    $scope.searchiTunes = function(artist){
+        $http.jsonp('http://itunes.apple.com/search', {
+            params: {
+              'callback': 'JSON_CALLBACK',
+                'term': artist,
+                 'country': 'US',
+                 'media': 'music',
+                 'limit': 50,
+
+            }
+        }).then(onSearchComplete, onError)
+    }
+    var onSearchComplete = function(response){
+        $scope.data = response.data
+        $scope.songs = response.data.results
+    }
+
+    var onError = function(reason){
+        $scope.error = reason
+    }
+}
